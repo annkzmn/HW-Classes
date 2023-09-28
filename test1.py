f8210fa36f7d04cc932bd085f255dedbac9261b6
Anna Kuzmina <annkzmn@gmail.com>
2023-09-28 10:35:21 -0600

    feat: add test_get_users, .gitignore

--- test1.py
+++ test1.py
@@ -1,1 +1,22 @@
+import requests
+import pprint
+
 print('Hello')
+
+def test_first():
+    url = "https://send-request.me/api/users"
+    data = {
+        "first_name": "Giorgiana",
+        "last_name": "Diki",
+         "company_id": 1
+    }
+
+    response = requests.post(url, json=data)
+    print(response.json())
+    print(response.status_code)
+
+    def test_get_users():
+        url = "https://send-request.me/api/users/24627"
+        response = requests.get(url)
+        print(response.json())
+        print(response.status_code)
