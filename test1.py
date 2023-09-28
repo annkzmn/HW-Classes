9c442368e341b81b92ab07ccc069d4f8c99c0f3d
Anna Kuzmina <annkzmn@gmail.com>
2023-09-27 18:23:53 -0600

    feat: add test_first

--- test1.py
+++ test1.py
@@ -1,1 +1,15 @@
+import requests
+import pprint
+
 print('Hello')
+
+def test_first():
+    url = "https://send-request.me/api/users"
+    data = {
+        "first_name": "Giorgiana",
+        "last_name": "Diki",
+         "company_id": 1
+    }
+
+    response = requests.post(url, json=data)
+    print(response.json())
